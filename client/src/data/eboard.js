d5270998c7701530cf671f392bde03a2e3ca9161
trevorguttmann <trevorguttmann7@gmail.com>
2026-03-07 18:26:38 -0500

    Modified Eboard pages

---
+++ client/src/data/eboard.js
@@ -0,0 +1,57 @@
+export default [
+    {
+        title: "Chief Executive Officer",
+        name: "Angelina Ponce",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?_=20150327203541"
+    },
+    {
+        title: "Chief Integration Officer",
+        name: "Eric Kim",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?_=20150327203541"
+    },
+    {
+        title: "Chief Marketing Officer",
+        name: "Jaylin Man",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?_=20150327203541"
+    },
+    {
+        title: "Chief Operating Officer",
+        name: "Maxwell Chan",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?_=20150327203541"
+    },
+    {
+        title: "Chief Technology Officer",
+        name: "Dimitra Pando",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?_=20150327203541"
+    },
+    {
+        title: "Chief Financial Officer",
+        name: "Hilary Rojas Rosales",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?_=20150327203541"
+    },
+    {
+        title: "Director of Professional Development",
+        name: "Hamid Ajose",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?_=20150327203541"
+    },
+    {
+        title: "Director of Communications",
+        name: "Sarah Toniuk",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?_=20150327203541"
+    },
+    {
+        title: "Director of External Affairs",
+        name: "Mahin Mahmud",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?_=20150327203541"
+    },
+    {
+        title: "Senior Advisor",
+        name: "Aditya Kapoor",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?_=20150327203541"
+    },
+    {
+        title: "Founder | Senior Advisor",
+        name: "David Ponce",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?_=20150327203541"
+    }
+]
--- client/src/pages/Eboard.jsx
+++ client/src/pages/Eboard.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import eboard from "../data/eboard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const Eboard = () => {
+function Card(props) {
   return (
-    <div>Eboard</div>
-  )
+    <div className="px-3">
+      <div
+        className="
+        card
+        flex flex-col items-center gap-3
+        w-full
+        border border-gray-200
+        rounded-2xl
+        shadow-md
+        p-6
+        transition-all duration-300
+        hover:scale-105
+        hover:shadow-2xl
+        hover:border-accent
+        cursor-pointer"
+      >
+        <img src={props.image} />
+        <h1 className>{props.name}</h1>
+        <h3 className="text-accent text-center">{props.title}</h3>
+        <p>[Enter Description]</p>
+      </div>
+    </div>
+  );
 }
 
-export default Eboard
+const Eboard = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "0px",
+  };
+
+  return (
+    <div className="w-full px-10 py-10">
+      <Slider {...settings}>
+        {eboard.map((member, index) => (
+          <Card
+            key={index}
+            title={member.title}
+            name={member.name}
+            image={member.image}
+          />
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default Eboard;
